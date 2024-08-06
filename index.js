b2561762a6b4c25681527ce8b6001e87e0fbbbcf
hanney94 <hannahallan94@gmail.com>
2024-08-06 21:01:21 +0100

    Added Javascript for onClick function

---
+++ index.js
@@ -0,0 +1,7 @@
+function shareButton() {
+    document.querySelector(".share-popup").style.display = "block"; 
+}
+
+function closeButton () {
+    document.querySelector(".share-popup").style.display = "none"; 
+}
